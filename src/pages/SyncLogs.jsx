6dd43b9c94754f5df3c8fa898aa68c20f27b1118
Eslam Elsharkawy <58348866+aAbstract@users.noreply.github.com>
2024-01-18 16:44:43 +0200

    Update SyncLogs.jsx

--- src/pages/SyncLogs.jsx
+++ src/pages/SyncLogs.jsx
@@ -1,5 +1,40 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useEffect, useState } from 'react';
+import { getLogFiles, getLogs } from '../api/logs';
+import '../css/SyncLogs.css';
 
-const SyncLogs = () => <div>SyncLogs</div>;
+function saveFile(filename, fileContent) {
+    const blob = new Blob([fileContent], { type: 'text/plain' });
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.download = `${filename}.log`;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(a.href);
+}
+
+function logFileClick(filename, index) {
+    getLogs(index).then(axiosResp => {
+        const fileContent = axiosResp.data.data.logs;
+        saveFile(filename, fileContent);
+    });
+}
+
+const SyncLogs = () => {
+    const [logFiles, setLogFiles] = useState([]);
+
+    useEffect(() => {
+        getLogFiles().then(axiosRes => {
+            setLogFiles(axiosRes.data.data.files)
+        });
+    }, []);
+
+    return (
+        logFiles.length > 0
+            ? logFiles.map((file, index) => <div key={index} className='log-file-span' onClick={() => logFileClick(file, index)}>{file}</div>)
+            : <div>Loading Log Files...</div>
+    );
+};
 
 export default SyncLogs;
